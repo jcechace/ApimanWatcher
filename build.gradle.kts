15348efb0cf9779fc4f1e6ac98e478c4cce4b8bc
jcechace <jcechace@gmail.com>
2018-10-28 03:34:11 +0100

    Cleaned up build file and added logging dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.Coroutines
 val logback_version: String by project
 val ktor_version: String by project
 val kotlin_version: String by project
+val kotlin_logging_version: String by project
+val slf4j_version: String by project
 
 plugins {
     application
@@ -25,26 +27,28 @@ repositories {
 dependencies {
     compile(kotlin("stdlib", kotlin_version))
 
-    compile("io.ktor:ktor-server-cio:$ktor_version")
+//      http client
     compile("io.ktor:ktor-client-core:$ktor_version")
     compile("io.ktor:ktor-client-core-jvm:$ktor_version")
     compile("io.ktor:ktor-client-okhttp:$ktor_version")
-
     compile("io.ktor:ktor-client-auth-basic:$ktor_version")
     compile("io.ktor:ktor-client-json-jvm:$ktor_version")
     compile("io.ktor:ktor-client-gson:$ktor_version")
 
+//      json library
     compile("com.google.code.gson:gson:2.8.5")
 
+//      logging
+    compile ("io.github.microutils:kotlin-logging:$kotlin_logging_version")
+    compile("org.slf4j:slf4j-api:$slf4j_version")
     compile("ch.qos.logback:logback-classic:$logback_version")
 
-
+//      k8 client
     compile("io.fabric8:kubernetes-client:4.1.0")
     compile("io.fabric8:kubernetes-model:4.1.0")
     compile("io.fabric8:openshift-client:4.1.0")
 
+//      apiman dependencies
     compile("io.apiman:apiman-gateway-engine-beans:1.5.1.Final")
-
-
 }
 
