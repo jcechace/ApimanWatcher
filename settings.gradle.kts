81efb5306301a3c137a0fe98dcd6567306cf4dd5
jcechace <jcechace@gmail.com>
2018-11-01 13:11:37 +0100

    Removed obsolete plugin resolution from settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,4 @@ pluginManagement {
         maven(url = "https://kotlin.bintray.com/kotlin-eap")
         maven(url = "https://kotlin.bintray.com/kotlinx")
     }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "kotlinx-serialization") {
-                useModule("org.jetbrains.kotlin:kotlin-serialization:${requested.version}")
-            }
-        }
-    }
 }
