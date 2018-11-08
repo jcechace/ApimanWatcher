439518c8490edd54ec5f1ce3135c9edd799d6243
jcechace <jcechace@gmail.com>
2018-11-08 21:14:17 +0100

    Extended build script for docker image

--- docker/sidekick/build.sh
+++ docker/sidekick/build.sh
@@ -1,25 +1,40 @@
 #!/usr/bin/env bash
 
-OPTS=`getopt -o pn: --long push,name:  -- $@`
+OPTS=`getopt -o pnd: --long push,name:,dist:  -- $@`
 eval set -- "$OPTS"
 
 PUSH_TO_HUB=false
 IMAGE_LOCAL=jcechace/apiman-sidekick
+WORKSPACE=$(dirname "${BASH_SOURCE[0]}")
+DIST_SOURCE="${WORKSPACE}/../../build/distributions/sidekick-shadow.zip"
+
+# Error codes
+EXIT_DIST_NOT_FOUND=1
 
 while true; do
     case "$1" in
     -p | --push) PUSH_TO_HUB=true; shift ;;
     -n | --name) IMAGE_LOCAL=$2; shift; shift;;
+    -d | --dist) DIST_SOURCE=$2; shift; shift;;
     *) break ;;
     esac
 done
 
+
+if [ ! -f ${DIST_SOURCE} ]; then
+    echo "Distribution file not found!"
+    exit ${EXIT_DIST_NOT_FOUND}
+fi
+
+echo "Copying shadow distribution zip"
+cp ${DIST_SOURCE} .
+
 echo "Building docker image with tag '${IMAGE_LOCAL}'."
-docker build -t ${IMAGE_LOCAL} $( dirname "${BASH_SOURCE[0]}" )
+docker build -t ${IMAGE_LOCAL} ${WORKSPACE}
 
 
 if $PUSH_TO_HUB; then
-    echo "Pushing to docker hub."
+    echo "Pushing to docker hub.:"
     docker push ${IMAGE_LOCAL}:latest
 fi
 
